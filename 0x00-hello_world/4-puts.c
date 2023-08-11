23bbdf01952cd4beb451631ed28345f9ac034731
Modestus <mcakagha@gmail.com>
2023-08-11 12:32:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-char c = "\"Programming is like building a multilingual puzzle" | puts(c)
+#include <stdio.h>
+/* My puts() project */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
