a637740844c491234db94eeb8473c40fe28e5b84
Modestus <mcakagha@gmail.com>
2023-08-11 15:03:13 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - starts the program
+  * printf: prints out its content
+  * Return: 0 if it runs successfully
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
