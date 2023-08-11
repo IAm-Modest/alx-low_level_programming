b1e179811912a0f73bbb6f0605911ce3f3b5b73a
Modestus <mcakagha@gmail.com>
2023-08-11 13:37:36 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * main makes the beginning of the programme.
- *
+ * Return (0) Always successful.
  * puts() prints its content to the stdout 
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-/* Returns (0) if without error */
 	return (0);
 }
