d6d319289e118d85c1707c80f67317528b10c13e
Modestus <mcakagha@gmail.com>
2023-08-11 12:56:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**
- *  My puts() project 
+ * main makes the beginning of the programme.
+ *
+ * puts() prints its content to the stdout 
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+/* Returns (0) if without error */
 	return (0);
 }
