7354d948a4998abd4443848bea3d6a562b3e3a5d
Modestus <mcakagha@gmail.com>
2023-08-19 02:48:49 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - prints single digits
+  * Return: 0 if successful
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
