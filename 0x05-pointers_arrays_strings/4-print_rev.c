6477b57dedde13911268e41985016f4f17c1c39a
Modestus <mcakagha@gmail.com>
2023-08-23 07:59:10 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_rev - prints strings in reverse
+  * @s: string to be printed
+  */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (i != '\0')
+	{
+		i++;
+	}
+
+	for (i - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+
+}
