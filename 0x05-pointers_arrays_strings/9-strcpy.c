cc20110fad61f9b884ab36aa970e4b703c4b3d69
Modestus <mcakagha@gmail.com>
2023-08-27 09:19:22 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _strcpy - copies strings from src to dest
+  * @dest: the dest variable
+  * @src: holds the src variable
+  * Return: returns dest
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src
+
+	return (dest);
+}
