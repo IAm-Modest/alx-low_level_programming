8480c2115b6e512cddcac86b2b2bf64a5674bbae
Modestus <mcakagha@gmail.com>
2023-08-27 00:12:09 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * puts_half - prints the second half of a string
+  * @str: the string to be worked on
+  */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int len_2;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+	{
+		len_2 = len / 2;
+	} else
+	{
+		len_2 = (len - 1) / 2;
+	}
+
+	for (; len_2 <= len; len_2++)
+		_putchar(str[len_2];
+	_putchar('\n');
+}
+
