f5428ba82714d884c4bf306d0653b1baebe62360
Modestus <mcakagha@gmail.com>
2023-08-28 14:44:09 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * _strpbrk - searches for any of a set of bytes
+  * @s: string
+  * @accept: bytes
+  * Return: pointer to the byte in s
+  */
+
+char *_strpbrk(char *s, char *accept)
+{
+	return (s);
+}
