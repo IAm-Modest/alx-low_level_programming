6c84634dd848d932794caec9e831c23bcd791426
Modestus <mcakagha@gmail.com>
2023-08-28 14:26:49 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * _strspn - gets teh length of a prefix substring
+  * @s: prefix source string
+  * @accept: bytes
+  * Return: number of bytes
+  */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	return (0);
+}
