99680e54874384bd4d3be52a06d09d4a5d6325a0
Modestus <mcakagha@gmail.com>
2023-08-28 13:49:20 -0700

    new files

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _memset - fills memory with bytes
+  * @s: pointer
+  * @b: character to fill
+  * @n: number of values to fill
+  * Return: 0
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (0);
+}
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _memcpy - copies the memory of a pointer
+  * @dest: the destination pointer
+  * @src: source pointer
+  * @n: number of arrays to copy
+  * Return: 0
+  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (0);
+}
