f9669bcb25c76e00064b315fc31842751ae61192
Modestus <mcakagha@gmail.com>
2023-08-29 08:32:28 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,16 +10,16 @@
 char *_strcat(char *dest, char *src)
 {
 	int len1 = 0;
-	int len2;
+	int len2 = 0;
 
 	while (*dest[len1] != '\0')
 		len1++;
 
-	for (len2 = 0; src[len2] != '\0'; len2++)
+	while (src[len2] != '\0')
 	{
-		*dest[len1 + len2] = *src[len2];
+		dest[len1 + len2] = src[len2];
 	}
-	*dest[len1 + len2] = '\0';
+	dest[len1 + len2] = '\0';
 
-	return (*dest);
+	return (dest);
 }
