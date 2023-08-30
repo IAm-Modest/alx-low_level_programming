a10910ae5156b88e5ad129871cdb88b7b93f7408
Modestus <mcakagha@gmail.com>
2023-08-29 19:43:34 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,14 +5,14 @@
   * @s: pointer
   * @b: character to fill
   * @n: number of values to fill
-  * Return: 0
+  * Return: return pointer to memory
   */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	int i;
 
-	for (n = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
