cdcfcec9d586547b14eb7d0406edaf50647ba775
Modestus <mcakagha@gmail.com>
2023-08-31 03:55:24 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _puts_recursion - prints a string followed by a newline
+  * @s: string to be printed
+  */
+
+void _puts_recursion(char *s)
+{
+	int n = 0;
+
+	_putchar(s[n]);
+
+	if (s[n] != '\0')
+	{
+		n++;
+		_puts_recursion(&s[n]);
+	}
+	else
+	{
+		_Putchar('\n');
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This directory holds my ALX recursion projects
