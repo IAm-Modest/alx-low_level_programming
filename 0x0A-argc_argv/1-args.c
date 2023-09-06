7b98780d6677f1c4b41ea204b2ae19d01c29d51d
Modestus <mcakagha@gmail.com>
2023-09-06 06:11:10 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,9 @@
   * Return: 0 if successful
   */
 
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
+	(void)argv;
 	printf("%d\n", argc);
 
 	return (0);
