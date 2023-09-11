492cde98017e09daaabea73e3db3d8539e93bb64
Modestus <mcakagha@gmail.com>
2023-09-11 07:54:12 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory contains my ALX projects on Structure and typedef in C programming
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - a structure
+  * @name: string type
+  * @age: float type
+  * @owner: string type
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
